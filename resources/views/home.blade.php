64f56a730f375a3e708c5f8a4192d0f3554ec1c3
unknown <nelsonferolie>
2024-08-14 18:44:06 +0700

    lokasi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,48 @@
 
 @section('title', 'SMA Abdiwacana Pontianak')
 
- @section('konten')
- 
-    <div class="banner">
-     <img src={{ asset('assets/img/banner.png') }} class="rounded mx-auto d-block" alt="SMA Abdiwacana Pontianak" >
+@section('konten')
+    <div>
+        <div class="banner">
+            <img src="{{ asset('assets/img/banner.png') }}" class="rounded mx-auto d-block" alt="SMA Abdiwacana Pontianak">
+        </div>
+        <div class="p-5">
+            <div style="color:#303030;">
+                <p class="text-center" style="font-size: 30px; font-weight:500">Ekstrakulikuler</p>
+                <div class="container"
+                    style="text-align: center; display: flex; justify-content: center; align-items: center; flex-direction: column; margin-bottom: 20px">
+                    <div class="column" style="display: flex; flex-direction: row; gap: 20px;">
+                        <div style="text-align: center; font-size: 20px; font-weight:600">
+                            <img src="{{ asset('assets/img/img1.jpeg') }}" class="rounded" alt="Image 1" width="410">
+                            <p>Futsal</p>
+                        </div>
+                        <div style="text-align: center; font-size: 20px; font-weight:600 ">
+                            <img src="{{ asset('assets/img/bg-title.png') }}" class="rounded" alt="Image 3" width="410"
+                                height="265">
+                            <p>Paskibra</p>
+                        </div>
+                        <div style="text-align: center; font-size: 20px; font-weight:600 ">
+                            <img src="{{ asset('assets/img/img2.jpeg') }}" class="rounded" alt="Image 2" width="410">
+                            <p>Badminton</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <p class="text-center " style="font-size: 30px; font-weight:500">Alamat Sekolah</p>
+                <div style="display: flex; justify-content: center; align-items: center; flex-direction: column">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.8171995034454!2d109.33676507472333!3d-0.04294699995662636!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e1d59af992a779d%3A0x42a54f65442ecb37!2sSMP-SMA%20Kristen%20Abdiwacana!5e0!3m2!1sid!2sid!4v1723633783938!5m2!1sid!2sid"
+                        width="1280" height="500" style="border:0; rounded" allowfullscreen="" loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <p style="font-size: 20px; font-weight: 300">Jl. Jenderal Ahmad Yani No.52 F,
+                        Benua Melayu Darat, Kec.
+                        Pontianak Sel.,
+                        Kota Pontianak, Kalimantan
+                        Barat 78121</p>
+                </div>
+            </div>
+        </div>
     </div>
-    </div>
- 
+
 @endsection
--- resources/views/profilesekolah.blade.php
+++ resources/views/profilesekolah.blade.php
@@ -4,31 +4,32 @@
 @section('title', 'Visi dan Misi')
 
 @section('konten')
-  
+
     <div style="background-image: url(assets/img/bg-title.png)">
         <div style="padding:110px;">
             {{-- <h1 class="text-center text-white fs-800 fw-semibold"><strong>Visi dan Misi</strong></h1> --}}
         </div>
     </div>
     <div style="background-image: url(assets/img/bg2.jpg)">
-    <div class="text-center pb-1 p-5">
-        <h2 class="fw-bold pb-2">VISI</h2>
-        <P>“Mewujudkan sumber daya manusia yang didasari firman tuhan dengan semangat cinta kasih, bersekutu, melayani dan bersaksi.”</P>
+        <div class="text-center pb-1 p-5">
+            <h2 class="fw-bold pb-2">VISI</h2>
+            <P>“Mewujudkan sumber daya manusia yang didasari firman tuhan dengan semangat cinta kasih, bersekutu, melayani
+                dan bersaksi.”</P>
+        </div>
+
+        <div class="pt-2 px-5 pb-1">
+            <h2 class="text-center fw-bold pb-2">MISI</h2>
+            <ol style="margin-left: 430px;" class="text-justify ol" type="1">
+                <li>Melaksanakan proses belajar mengajar dalam semangat cinta kasih.</li>
+                <li>Menjadikan sekolah sebagai pusat kesaksian, persekutuan dan pelayanan..</li>
+                <li>Menjadikan warga sekolah yang kreatif, inovatif, dan berkembang.</li>
+                <li>Memotivasi siswa untuk berkembang menempa diri sesuai dengan talenta yang dimiliki.</li>
+                <li>Mampu bersaing secara kompetitif dalam ilmu pengetahuan dan teknologi.</li>
+                <li>Menjadikan sekolah sebagai saluran berkat.</li>
+                <li>Menjadikan sekolah sebagai garam dan terang.</li>
+            </ol>
+        </div>
     </div>
 
-    <div class="pt-2 px-5 pb-1" >
-        <h2  class="text-center fw-bold pb-2">MISI</h2>
-        <ol style="margin-left: 430px;" class="text-justify ol" type="1">
-            <li>Melaksanakan proses belajar mengajar dalam semangat cinta kasih.</li>
-            <li>Menjadikan sekolah sebagai pusat kesaksian, persekutuan dan pelayanan..</li>
-            <li>Menjadikan warga sekolah yang kreatif, inovatif, dan berkembang.</li>
-            <li>Memotivasi siswa untuk berkembang menempa diri sesuai dengan talenta yang dimiliki.</li>
-            <li>Mampu bersaing secara kompetitif dalam ilmu pengetahuan dan teknologi.</li>
-            <li>Menjadikan sekolah sebagai saluran berkat.</li>
-            <li>Menjadikan sekolah sebagai garam dan terang.</li>
-        </ol>
-    </div>
-    </div>
 
-  
 @endsection
